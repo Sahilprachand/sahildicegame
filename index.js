04bd1200a0d4ef98e95e591958a97ca711eb186f
Sahilprachand <prachandsahil@gmail.com>
2025-07-11 01:10:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" +randomNumber1 +".png";
+
+var randomImageSource ="images/" + randomDiceImage;
+
+var image1 =document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource );
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" +randomNumber2 +".png";
+
+var randomImageSource ="images/" + randomDiceImage;
+
+var image1 =document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImageSource );
+
+
+
+if (randomNumber1 > randomNumber2){
+
+
+    document.querySelector("h1").innerHTML="player 1 won";
+}else if (randomNumber2 > randomNumber1){
+document.querySelector("h1").innerHTML="player 2 won";
+}else{
+    document.querySelector("h1").innerHTML="its a draw";
+}
